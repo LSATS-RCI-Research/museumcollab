5c793811eb993df520ff3d75a8ef28931b3004c9
Joe Bauer <joebauer@users.noreply.github.com>
2023-06-23 07:45:14 -0400

    Update mv-files-php-pre-start.sh
    file path didn't seem to match up with where the v4 builder was putting files.

--- php-pre-start/mv-files-php-pre-start.sh
+++ php-pre-start/mv-files-php-pre-start.sh
@@ -3,7 +3,7 @@
 # see ./php-pre-start documentation
 # https://catalog.redhat.com/software/containers/ubi8/php-73/5d400891bed8bd38099104e0?gti-tabs=unauthenticated
 
-MOD_ROOT='/opt/app-root/src/php-pre-start'
+MOD_ROOT='/opt/app-root/src/'
 APP_ROOT='/opt/app-root/src/omeka-s'
 
 mv -v $MOD_ROOT/config/local.config.php $APP_ROOT/config/local.config.php
